4071c36d4caf7e3ab2f6b3ea5116257eaf5e16ec
attila <apatel@aviatrix.com>
2024-09-26 19:39:41 +0100

    push

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # https://aviatrix.atlassian.net/wiki/spaces/SEA/pages/1779302805/Fortinet
 # Bootstrap - https://read.docs.aviatrix.com/HowTos/fortigate_bootstrap_example_azure.html
 
-/*
+
 # (A)  Transit + firenet
 module "mc_transit" {
   source                 = "terraform-aviatrix-modules/mc-transit/aviatrix"
@@ -60,4 +60,3 @@ data "aviatrix_firenet_vendor_integration" "fw1" {
   depends_on = [ time_sleep.wait_90_seconds ]
 
 }
-*/
