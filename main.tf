2067b606266b0a393ae54432a242243fb60a362f
attila <apatel@aviatrix.com>
2024-10-03 19:04:06 +0100

    destroy

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # https://aviatrix.atlassian.net/wiki/spaces/SEA/pages/1779302805/Fortinet
 # Bootstrap - https://read.docs.aviatrix.com/HowTos/fortigate_bootstrap_example_azure.html
 
-
+/*
 # (A)  Transit + firenet
 module "mc_transit" {
   source                 = "terraform-aviatrix-modules/mc-transit/aviatrix"
@@ -18,12 +18,11 @@ module "mc_transit" {
   resource_group = "atulrg-customiam"               # If using SP at RG level
   learned_cidr_approval = true
   learned_cidrs_approval_mode = "gateway"
-  #enable_egress_transit_firenet = false
+  #enable_egress_transit_firenet = false          # for egress g/w
   connected_transit = "true"  #  needed to remove egress ^^
 }
 
 
-/*
 module "firenet_1" {
   source         = "terraform-aviatrix-modules/mc-firenet/aviatrix"
   version        = "1.5.0"
